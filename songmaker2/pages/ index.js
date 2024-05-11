671dee82d330b6c7a1a28f0adea5a14b688c6e73
Serena <116518998+Serenwfs@users.noreply.github.com>
2024-05-11 15:10:57 -0700

    Delete songmaker2 directory

--- songmaker2/pages/ index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import HomePage from '../components/HomePage';  // Adjust the import path based on your file structure
-
-export default function Home() {
-  return <HomePage />;
-}
--- songmaker2/pages/_app.js
+++
@@ -1,7 +0,0 @@
-// import '../src/globals.css'; // Assuming you have global styles you want to import
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
